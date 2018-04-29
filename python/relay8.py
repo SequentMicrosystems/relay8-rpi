3512d85639e9ad6c3e587ba5b53eaff20d5e9fac
Alexandru Burcea <olcitu@gmail.com>
2018-04-29 23:00:33 +0300

    Create relay8.py

---
+++ python/relay8.py
@@ -0,0 +1,5 @@
+import smbus
+
+#bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+
+DEVICE_ADDRESS = 0x31     #7 bit address (will be left shifted to add the read write bit)
