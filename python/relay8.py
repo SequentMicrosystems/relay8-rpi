27290cf78b96247e6bfe79828a647d860f561d7e
alexburcea2877 <olcitu@gmail.com>
2018-05-31 23:22:55 +0000

    Changes for hardware v2

--- python/relay8.py
+++ python/relay8.py
@@ -9,6 +9,24 @@ RELAY8_OUTPORT_REG_ADD = 0x01
 RELAY8_POLINV_REG_ADD = 0x02
 RELAY8_CFG_REG_ADD = 0x03
 
+relayMaskRemap = [0x01, 0x02, 0x04, 0x08, 0x80, 0x40, 0x20, 0x10] 
+relayChRemap = [0, 1, 2, 3, 7, 6, 5, 4]
+
+
+def relayToIO(relay):
+	val = 0
+	for i in range(0, 8):
+		if (relay & (1 << i)) != 0:
+			val = val + relayMaskRemap[i]
+	return val
+  
+def IOToRelay(iov):
+	val = 0
+	for i in range(0, 8):
+		if (iov & relayMaskRemap[i]) != 0:
+			val = val + (1<< i)
+	return val
+
 def check(bus, add):
 	cfg = bus.read_byte_data(add, RELAY8_CFG_REG_ADD)
 	if(cfg != 0):
@@ -22,18 +40,23 @@ def set(stack, relay, value):
 	if stack < 0 or stack > 7:
 		raise ValueError('Invalid stack level')
 		return
+  
+	stack = 0x07 ^ stack;
 	if relay < 1:
 		raise ValueError('Invalid relay number')
 		return
 	if relay > 8:
 		raise ValueError('Invalid relay number')
 		return
-	oldVal = check(bus, DEVICE_ADDRESS + stack)	
+	oldVal = check(bus, DEVICE_ADDRESS + stack)
+	oldVal = IOToRelay(oldVal)
 	if value == 0:
 		oldVal = oldVal & (~(1 << (relay - 1)))
+		oldVal = relayToIO(oldVal)
 		bus.write_byte_data(DEVICE_ADDRESS + stack, RELAY8_OUTPORT_REG_ADD, oldVal)
 	else:
 		oldVal = oldVal | (1 << (relay - 1))
+		oldVal = relayToIO(oldVal)
 		bus.write_byte_data(DEVICE_ADDRESS + stack, RELAY8_OUTPORT_REG_ADD, oldVal)
 
 		
@@ -42,6 +65,7 @@ def set_all(stack, value):
 	if stack < 0 or stack > 7:
 		raise ValueError('Invalid stack level')
 		return
+	stack = 0x07 ^ stack  
 	if value > 255 :
 		raise ValueError('Invalid relay value')
 		return
@@ -49,6 +73,7 @@ def set_all(stack, value):
 		raise ValueError('Invalid relay value')
 		return
 	oldVal = check(bus, DEVICE_ADDRESS + stack)	
+	value = relayToIO(value)
 	bus.write_byte_data(DEVICE_ADDRESS + stack, RELAY8_OUTPORT_REG_ADD, value)
 
 
@@ -58,6 +83,7 @@ def get(stack, relay):
 	if stack < 0 or stack > 7:
 		raise ValueError('Invalid stack level')
 		return
+	stack = 0x07 ^ stack  
 	if relay < 1:
 		raise ValueError('Invalid relay number')
 		return
@@ -65,6 +91,7 @@ def get(stack, relay):
 		raise ValueError('Invalid relay number')
 		return
 	val = check(bus, DEVICE_ADDRESS + stack)
+	val = IOToRelay(val) 
 	val = val & (1 << (relay - 1))
 	if val == 0:
 		return 0
@@ -78,4 +105,7 @@ def get_all(stack):
 	if stack < 0 or stack > 7:
 		raise ValueError('Invalid stack level')
 		return
-	return check(bus, DEVICE_ADDRESS + stack)
+	stack = 0x07 ^ stack
+	val = check(bus, DEVICE_ADDRESS + stack)
+	val = IOToRelay(val) 
+	return val
